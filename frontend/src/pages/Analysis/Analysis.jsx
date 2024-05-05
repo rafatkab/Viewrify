42b098d8011ff591f93c018dd65a11c2935f5574
rafatkab <rafat.kabir@outlook.com>
2024-05-04 21:40:20 -0400

    e

--- frontend/src/pages/Analysis/Analysis.jsx
+++ frontend/src/pages/Analysis/Analysis.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Analysis() {
-  return <div>Analysis</div>;
+  const [transcript, setTranscript] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Make a fetch request to your backend API
+        const url = localStorage.getItem("url");
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/${url.substring(30)}`
+        );
+
+        // Check if the response is successful
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        // Parse the JSON response
+        const result = await response.json();
+
+        // Update the state with the fetched data
+        setTranscript(result);
+      } catch (error) {
+        // Handle any errors
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>{transcript}</div>;
 }
